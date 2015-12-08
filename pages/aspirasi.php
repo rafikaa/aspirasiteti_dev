77791c6ca2c58d2a6d70625c2641815040905224
rafikaa <aminirafika@gmail.com>
2015-12-08 11:33:03 +0700

    frontend detail aspirasi fixed

--- pages/aspirasi.php
+++ pages/aspirasi.php
@@ -5,12 +5,69 @@
 	<div class="conainer">
 		<div class="col-lg-10 col-lg-offset-1 box-detail-saran">
 			<div class="col-lg-8 box-detail-saran-kiri">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<div class="col-lg-8 no-padding">
+					<div class="col-lg-2 no-padding">
+						<img class="img-profil-lg" src="<?php echo BASE_URL; ?>img/profil.png">
+					</div>
+					<div class="col-lg-10 no-padding">
+						<h5 class="nama" style="margin-top:15px">Afgansyah Reza</h5>
+						<h6 class="status" >Mahasiswa</h6>
+					</div>
+				</div>
+				<div class="col-lg-4 no-padding">
+					<label class="label-bidang-lg" style="margin-top:15px">KMTETI</label>
+				</div>
+				<div class="col-lg-12">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
 			</div>
 			<div class="col-lg-4 box-detail-saran-kanan">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<div class="col-lg-12">
+					<div class="col-lg-6 aspirasi-desc no-padding">
+						<h5>8 jam lalu / 7 suara</h5>
+					</div>
+					<div class="col-lg-6 aspirasi-aksi no-padding">
+						<button class="btn btn-aksi"><span class="glyphicon glyphicon-ok"></span></button>
+						<button class="btn btn-aksi"><span class="glyphicon glyphicon-remove"></span></button>
+						<button class="btn btn-aksi"><span class="glyphicon glyphicon-heart"></span></button>
+					</div>
+				</div>
+				<div class="col-lg-12 box-last-action">
+					<div class="col-lg-2 no-padding">
+						<img class="img-profil" src="<?php echo BASE_URL; ?>img/profil.png">
+					</div>
+					<div class="col-lg-10 no-padding">
+						<h5 class="last-action">Afgansyah Reza menyukai ini</h5>
+					</div>
+				</div>
+				<div class="col-lg-12 box-last-action">
+					<div class="col-lg-2 no-padding">
+						<img class="img-profil" src="<?php echo BASE_URL; ?>img/profil.png">
+					</div>
+					<div class="col-lg-10 no-padding">
+						<h5 class="last-action">Afgansyah Reza menyukai ini</h5>
+					</div>
+				</div>
+				<div class="col-lg-12 box-last-action">
+					<div class="col-lg-2 no-padding">
+						<img class="img-profil" src="<?php echo BASE_URL; ?>img/profil.png">
+					</div>
+					<div class="col-lg-10 no-padding">
+						<h5 class="last-action">Afgansyah Reza menyukai ini</h5>
+					</div>
+				</div>
+				<div class="col-lg-12 box-last-action">
+					<div class="col-lg-2 no-padding">
+						<img class="img-profil" src="<?php echo BASE_URL; ?>img/profil.png">
+					</div>
+					<div class="col-lg-10 no-padding">
+						<h5 class="last-action">Afgansyah Reza menyukai ini</h5>
+					</div>
+				</div>
 			</div>
 			
 		</div>
